28392214145818681465579d2f490acdd56d21d2
dalgim <dalgiewiczmateusz@gmail.com>
2017-04-16 14:46:11 +0200

    fixing

--- sb-cl-cxf-wss-signature-client/src/test/java/com/dalgim/example/sb/cxf/cxfwsssignature/SbClCxfWssSignatureClientApplicationTests.java
+++ sb-cl-cxf-wss-signature-client/src/test/java/com/dalgim/example/sb/cxf/cxfwsssignature/SbClCxfWssSignatureClientApplicationTests.java
@@ -1,7 +1,9 @@
-package com.dalgim.example.sb.cxf;
+package com.dalgim.example.sb.cxf.cxfwsssignature;
 
+import com.dalgim.example.sb.cxf.cxfwsssignature.endpoint.FruitService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SbClCxfWssSignatureClientApplicationTests {
 
+	@Autowired
+	private FruitService fruitService;
+
 	@Test
 	public void contextLoads() {
+		fruitService.getAllFruit();
 	}
 
 }
---
+++ sb-cl-cxf-wss-signature-server/src/main/java/com/dalgim/example/sb/cxf/cxfwsssignature/endpoint/FruitService.java
@@ -0,0 +1,16 @@
+package com.dalgim.example.sb.cxf.endpoint;
+
+import com.dalgim.example.sb.cxf.model.Fruit;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import java.util.Collection;
+
+/**
+ * Created by dalgim on 08.04.2017.
+ */
+@WebService
+public interface FruitService {
+
+    @WebMethod
+    Collection<Fruit> getAllFruit();
+}
